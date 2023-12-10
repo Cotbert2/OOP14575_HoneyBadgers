7f407b88f22fa34be58d4b65caba2573924ed11b
Cotbert <mateojgdev@gmail.com>
2023-12-09 19:59:34 -0500

    test commit

--- 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/model/ProductItem.java
+++ 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/model/ProductItem.java
@@ -6,7 +6,7 @@ package ec.edu.espe.viveresgabysoftwarekit.model;
  * @author mateo, Stefany Díaz
  */
 class ProductItem {
-        private int product;
+    private int product;
     private int units;
     public void applyDiscount(Discount discount){};
 }
