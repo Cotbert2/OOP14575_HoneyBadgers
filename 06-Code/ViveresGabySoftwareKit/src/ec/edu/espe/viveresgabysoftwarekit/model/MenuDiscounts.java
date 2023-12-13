f3fa9e309035f893914a7e8abb0d83ed7fd9fee5
eduardo2704garcia <eduagarcia985@gmail.com>
2023-12-12 22:37:28 -0500

    menu V1.2.0

--- 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/model/MenuDiscounts.java
+++ 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/model/MenuDiscounts.java
@@ -1,70 +1,67 @@
 package ec.edu.espe.viveresgabysoftwarekit.model;
+
+import ec.edu.espe.viveresgabysoftwarekit.utils.Validations;
+import java.util.Random;
 import java.util.Scanner;
 
-/**
- *
- * @author Stefany Díaz
- */
-public class MenuDiscounts{
-    
+public class MenuDiscounts {
+
     private static Scanner scanner = new Scanner(System.in);
-    int option;
-    public void handMenuDiscounts(){
-        do{
-            System.out.println("----Discounts Menu----");
+    private int option;
+
+    public void handMenuDiscounts() {
+        do {
+            System.out.println("---- Discounts Menu ----");
             System.out.println("1. History");
             System.out.println("2. Create Discount");
             System.out.println("3. Delete Discount");
             System.out.println("4. Return to principal menu");
             System.out.print("Choose an option: ");
-            
-        option = getOption();
 
-            switch(option){
-                 case 1:
+            option = getOption();
+
+            switch (option) {
+                case 1:
                     System.out.println("You selected History");
                     doHistoryAction();
                     break;
-                    
+
                 case 2:
                     System.out.println("You selected Create Discount");
                     doCreateDiscountAction();
                     break;
-                    
+
                 case 3:
                     System.out.println("You selected Delete Discount");
                     doDeleteDiscountAction();
                     break;
-                    
+
                 case 4:
                     System.out.println("Leaving to the principal menu...");
                     break;
-                            
+
                 default:
                     System.out.println("Try again, invalid option");
             }
-        }while(option !=4);
-
+        } while (option != 4);
     }
 
-    private static int getOption(){
-           while(true){
+    private int getOption() {
+        while (true) {
             try {
-                 int input = Integer.parseInt(scanner.nextLine());
-                if (input >= 1 && input <= 9) {
+                int input = Integer.parseInt(scanner.nextLine());
+                if (input >= 1 && input <= 4) {
                     return input;
                 } else {
                     System.out.print("Invalid option, try again: ");
                 }
-            } catch (NumberFormatException e){
-                System.out.print("Invalid entry, try again: ");     
+            } catch (NumberFormatException e) {
+                System.out.print("Invalid entry, try again: ");
             }
-        
         }
-    
     }
-    
-    private void doHistoryAction(){
+
+    private void doHistoryAction() {
         System.out.println("Discounts History");
         System.out.println("---- Discounts History Menu ----");
         System.out.println("1. View History");
@@ -77,15 +74,43 @@ public class MenuDiscounts{
             System.out.println("Going back to Financer Menu...");
         } else {
             System.out.println("Invalid option, try again");
+        }
     }
-}
-    
 
-    private void doCreateDiscountAction(){
+    private void doCreateDiscountAction() {
         System.out.println("Discount - Create");
+
+        System.out.print("Do you want to add a discount manually? (1 for Yes, 2 for No): ");
+        int manualOption = getOption();
+
+        if (manualOption == 1) {
+            System.out.print("Enter the date (Father's Day, Mother's Day, Christmas, New Year, Carnival): ");
+            String englishDate = Validations.validateStringInputWithSpaces("Enter the date: ");
+
+            System.out.print("Enter the discount value (between 2.00 and 8.00): ");
+            double discountValue = Validations.validateDoubleDiscountInput("Enter the discount value: ");
+
+            System.out.println("Creating discount for " + englishDate + ": " + String.format("%.2f", discountValue) + "%");
+            // Puedes almacenar esta información en una estructura de datos o base de datos según tus necesidades
+        } else if (manualOption == 2) {
+            generateAutomaticDiscounts();
+        } else {
+            System.out.println("Invalid option, returning to the main menu.");
+        }
     }
 
-    private void doDeleteDiscountAction(){
+    private void generateAutomaticDiscounts() {
+        String[] dates = {"Father's Day", "Mother's Day", "Christmas", "New Year", "Carnival"};
+        Random random = new Random();
+
+        for (String date : dates) {
+            double randomDiscount = 2.00 + (random.nextDouble() * (8.00 - 2.00)); // Generar descuento aleatorio entre 2% y 8%
+            System.out.println("Creating discount for " + date + ": " + String.format("%.2f", randomDiscount) + "%");
+            // Puedes almacenar esta información en una estructura de datos o base de datos según tus necesidades
+        }
+    }
+
+    private void doDeleteDiscountAction() {
         System.out.println("Discount Delete");
         System.out.println("---- Delete Discount Menu ----");
         System.out.println("0. Go Back");
@@ -99,4 +124,3 @@ public class MenuDiscounts{
         }
     }
 }
-    
--- 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/view/ViveresGabySoftwareKit.java
+++ 06-Code/ViveresGabySoftwareKit/src/ec/edu/espe/viveresgabysoftwarekit/view/ViveresGabySoftwareKit.java
@@ -87,4 +87,3 @@ public class ViveresGabySoftwareKit {
         }
     }
 }
-//clean code
