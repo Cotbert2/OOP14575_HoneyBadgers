222a1f7666a975d46f53c59695c9a2fcec332621
Cotbert <mateojgdev@gmail.com>
2024-01-31 03:35:50 -0500

    V0.1.0

--- 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/helpers/Constans.java
+++ 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/helpers/Constans.java
@@ -6,24 +6,29 @@ package ec.edu.espe.viveresgabysoftwarekit.helpers;
 
 public class Constans {
     public static final String OUTPUT_ROOT_FILE = "./output";
-    public static final String USERS_FILE_NAME = "./db/users.json";
-    public static final String PRODUCTS_FILE_NAME = "./db/products.json";
-    public static final String CUSTOMERS_FILE_NAME = "./db/customers.json";
+    public static final String USERS_FILE_NAME = "users";
+    public static final String PRODUCTS_FILE_NAME = "products";
+    public static final String CUSTOMERS_FILE_NAME = "customers";
     public static final String WELCOME_HEADER = "**************************************************\n" +
             "|  WELCOME TO VIVERES GABY SOFTWARE KIT           |\n" +
             "**************************************************";
     public static final String HEADER = "**************Viveres Gaby Software Kit**************";
-    public static final String BILLS_FILE_NAME = "./db/bills.json";
-    public static final String CATEGORIES_FILE_NAME = "./db/categories.json";
+    public static final String BILLS_FILE_NAME = "bills";
+    public static final String CATEGORIES_FILE_NAME = "categories";
 
-    public static final String DISCOUNTS_FILE_NAME = "./db/discounts.json";
-    public static final String TRANSACTION_FILE = "./db/transactions.json";
-    public static final String TAXES_FILE_NAME = "./db/taxes.json";
-    public static final String STOCK_FILE_NAME = "./db/stock.json";
+    public static final String DISCOUNTS_FILE_NAME = "discounts";
+    public static final String TRANSACTION_FILE = "transactions";
+    public static final String TAXES_FILE_NAME = "taxes";
+    public static final String STOCK_FILE_NAME = "stock";
     public static final String IVA_URL = "https://www.sri.gob.ec/impuesto-al-valor-agregado-iva";
 
     public static final String VIVERES_GABY_LANDING_PAGE = "https://658031b74f9e7f3203fb2d25--cheery-eclair-b46b9a.netlify.app/";
 
+    
+    
+    
+    public static final String DATA_BASE = "viveres_gaby_software_kit_DB";
+    public static final String URI = "mongodb+srv://mateo:mateo@cluster0.jp3jhno.mongodb.net/?retryWrites=true&w=majority";
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
--- 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/utils/FileHandler.java
+++ 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/utils/FileHandler.java
@@ -2,6 +2,9 @@ package ec.edu.espe.viveresgabysoftwarekit.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 import java.io.*;
 import java.lang.reflect.ParameterizedType;
@@ -9,40 +12,48 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import ec.edu.espe.viveresgabysoftwarekit.controller.Db;
+import ec.edu.espe.viveresgabysoftwarekit.model.Product;
+import org.bson.Document;
+
+import ec.edu.espe.viveresgabysoftwarekit.helpers.Constans;
+import java.util.stream.Collectors;
+
 /**
- * @autor Alex Cuzco, Stefany Díaz, Eduardo García, Matego García-HONEYBUDGERS-DCCO-14575
+ * @autor Alex Cuzco, Stefany Díaz, Eduardo García, Matego
+ * García-HONEYBUDGERS-DCCO-14575
  */
 
 public class FileHandler<T> {
 
-    public List<T> readJSONListGeneric(String path, Class<T> classTarget) {
-        List<T> objectList = new ArrayList<>();
+    public List<T> readJSONListGeneric(String collectionName, Class<T> classTarget) {
+        Db.conectToDb();
 
-        try (Reader reader = new FileReader(path)) {
-            Type listType = new ListParameterizedType(classTarget);
-            objectList = new Gson().fromJson(reader, listType);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        MongoDatabase database = Db.mongoClient.getDatabase(Constans.DATA_BASE);
+        MongoCollection<Document> collection = database.getCollection(collectionName);
 
-        if (objectList == null) {
-            objectList = new ArrayList<>();
-            this.saveJSONFile(objectList, path);
-            this.readJSONListGeneric(path, classTarget);
+        List<Document> fullTable = collection.find().into(new ArrayList<>());
+        List<T> customersFromDataBase = new ArrayList<>();
+
+        for (Document doc : fullTable) {
+            Type type = TypeToken.getParameterized(classTarget).getType();
+
+            customersFromDataBase.add(new Gson().fromJson(doc.toJson(), type));
         }
-        return objectList;
+        return customersFromDataBase;
     }
 
+    public void saveJSONFile(List<T> templateList, String collectionName) {
+        Db.mongoClient.getDatabase(Constans.DATA_BASE).getCollection(collectionName).drop();
 
-    public void saveJSONFile(List<T> templateList, String path) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        try (FileWriter writer = new FileWriter(path)) {
-            writer.write(gson.toJson(templateList));
-            System.out.println("[+]Data Saved Succesfully.");
-        } catch (IOException e) {
-            System.err.println("[-] Something went wrong: " + e.getMessage());
-        }
+        gson.toJson(templateList);
+        List<Document> documents = templateList.stream().map(template -> Document.parse(gson.toJson(template))).collect(Collectors.toList());
+        
+        Db.mongoClient.getDatabase(Constans.DATA_BASE).getCollection(collectionName).insertMany(documents);
     }
+    
+    
 
     public void saveTXTFile(String data, String path) {
         try {
@@ -56,12 +67,12 @@ public class FileHandler<T> {
         }
     }
 
-
     public String getDesktopPath() {
         return System.getProperty("user.home") + "/Desktop";
     }
 
     private static class ListParameterizedType implements ParameterizedType {
+
         private final Type type;
 
         private ListParameterizedType(Class<?> type) {
