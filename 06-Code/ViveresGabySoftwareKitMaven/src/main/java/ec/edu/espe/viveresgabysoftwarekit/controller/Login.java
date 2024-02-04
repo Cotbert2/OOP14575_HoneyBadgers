435ea3291c56dc28f01d71ec03fde419a0224767
Cotbert <mateojgdev@gmail.com>
2024-02-04 14:05:02 -0500

    UI implementation V0.2.0

---
+++ 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/controller/Login.java
@@ -0,0 +1,35 @@
+
+package ec.edu.espe.viveresgabysoftwarekit.controller;
+
+import ec.edu.espe.viveresgabysoftwarekit.helpers.Constans;
+import ec.edu.espe.viveresgabysoftwarekit.model.User;
+import ec.edu.espe.viveresgabysoftwarekit.utils.Cypher;
+import ec.edu.espe.viveresgabysoftwarekit.utils.DbManager;
+import ec.edu.espe.viveresgabysoftwarekit.utils.Validator;
+import java.util.List;
+
+/**
+ *
+ * @author mateo
+ */
+public class Login {
+    Cypher cypher = new Cypher(17);
+    DbManager<User> DataBaseHandler = new DbManager<>();
+    
+    public boolean hasLogin(String user, String password){
+        
+        boolean authStatus = false;
+
+        List<User> users = DataBaseHandler.readJSONListGeneric(Constans.USERS_FILE_NAME, User.class);
+        
+        if(users.isEmpty())
+            System.out.println("No users registered");
+        for (User userItem : users) {
+            if (userItem.getUserName().equals(user) &&  userItem.getCypherPasswd() .equals(cypher.cypherMessage(password))) {
+                authStatus = true;
+                break;
+            }
+        }
+        return authStatus;
+    }
+}
--- 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/view/ViveresGabySoftwareKit.java
+++ 06-Code/ViveresGabySoftwareKitMaven/src/main/java/ec/edu/espe/viveresgabysoftwarekit/view/ViveresGabySoftwareKit.java
@@ -1,24 +1,25 @@
 package ec.edu.espe.viveresgabysoftwarekit.view;
 
 import ec.edu.espe.viveresgabysoftwarekit.helpers.Constans;
-import ec.edu.espe.viveresgabysoftwarekit.view.login.LogInSystem;
 import ec.edu.espe.viveresgabysoftwarekit.view.menus.MainMenu;
 
 import javax.mail.MessagingException;
 
-
-
-
 /**
- * @autor Alex Cuzco, Stefany Díaz, Eduardo García, Matego García-HONEYBUDGERS-DCCO-14575
+ * @autor Alex Cuzco, Stefany Díaz, Eduardo García, Matego
+ * García-HONEYBUDGERS-DCCO-14575
  */
-
 public class ViveresGabySoftwareKit {
+
     public static void main(String[] args) throws MessagingException {
-        System.out.println(Constans.WELCOME_HEADER);
+        FrmSplashScreen frmSplashScreen = new FrmSplashScreen();
+        frmSplashScreen.init();
+        
+        /*
         LogInSystem logInSystem = new LogInSystem();
         logInSystem.showLogin();
         MainMenu.showMenu();
+         */
     }
 
 }
